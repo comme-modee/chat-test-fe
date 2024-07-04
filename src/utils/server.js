12bb9c620ee824516f17a1b0599e5c371a6558bf
Soo Jung <comme746@naver.com>
2024-07-04 16:32:56 +0900

    update

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,5 +1,6 @@
 import { io } from 'socket.io-client'
-const REACT_APP_LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND
-const socket = io(REACT_APP_LOCAL_BACKEND)
+// const REACT_APP_LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND
+const REACT_APP_BACKEND = process.env.REACT_APP_BACKEND
+const socket = io(REACT_APP_BACKEND)
 
 export default socket;
